31f78f11cd3af8c4c6a2a7cd93fb0bee4c789c1b
Keijiro Takahashi <keijiro@gmail.com>
2022-02-04 15:52:30 +0900

    Xcode -> Makefile

---
+++ Plugin/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+LIB="KlakSyphon"
+DST="../Assets/Klak/Syphon/Internal"
+
+set -e
+set -x
+
+make clean
+make all TARGET="x86_64-apple-macos10.12"
+mv ${LIB}.dylib x86_64.dylib
+
+make clean
+make all TARGET="arm64-apple-macos10.12"
+mv ${LIB}.dylib arm64.dylib
+
+lipo -create -output ${LIB}.bundle x86_64.dylib arm64.dylib
+
+DST_FILE="${DST}/${LIB}.bundle"
+[ -e $DST_FILE ] && rm $DST_FILE
+cp ${LIB}.bundle $DST_FILE
