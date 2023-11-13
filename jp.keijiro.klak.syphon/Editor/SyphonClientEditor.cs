4e8aa234ec77f02fbb0ab4d02749532d618b1755
Keijiro Takahashi <keijiro@gmail.com>
2023-11-13 22:29:28 +0900

    Editor utilities from KalkNDI

--- jp.keijiro.klak.syphon/Editor/SyphonClientEditor.cs
+++ jp.keijiro.klak.syphon/Editor/SyphonClientEditor.cs
@@ -1,139 +1,67 @@
-// KlakSyphon - Syphon plugin for Unity
-// https://github.com/keijiro/KlakSyphon
-
 using UnityEngine;
 using UnityEditor;
-using System;
-using System.Collections.Generic;
 
-namespace Klak.Syphon
+namespace Klak.Syphon {
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(SyphonClient))]
+public class SyphonClientEditor : Editor
 {
-    [CanEditMultipleObjects]
-    [CustomEditor(typeof(SyphonClient))]
-    public class SyphonClientEditor : Editor
+    static class Labels
     {
-        SerializedProperty _appName;
-        SerializedProperty _serverName;
-        SerializedProperty _targetTexture;
-        SerializedProperty _targetRenderer;
-        SerializedProperty _targetMaterialProperty;
+        public static Label Property = "Property";
+        public static Label Select = "Select";
+    }
 
-        static GUIContent _labelProperty = new GUIContent("Property");
+    #pragma warning disable CS0649
 
-        string[] _propertyList; // Cached property list
-        Shader _cachedShader;   // Shader stored in the cache
+    AutoProperty _appName;
+    AutoProperty _serverName;
+    AutoProperty _targetTexture;
+    AutoProperty _targetRenderer;
+    AutoProperty _targetMaterialProperty;
 
-        // Retrieve the shader from the target renderer.
-        Shader RetrieveTargetShader(UnityEngine.Object target)
+    #pragma warning restore
+
+    void OnEnable() => AutoProperty.Scan(this);
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUI.BeginChangeCheck();
+
+        EditorGUILayout.DelayedTextField(_appName);
+        EditorGUILayout.DelayedTextField(_serverName);
+
+        // Force reconnection on modification to name properties.
+        if (EditorGUI.EndChangeCheck())
+            foreach (MonoBehaviour client in targets)
+                client.SendMessage("OnDisable");
+
+        // Target Texture/Renderer
+        EditorGUILayout.PropertyField(_targetTexture);
+        EditorGUILayout.PropertyField(_targetRenderer);
+
+        EditorGUI.indentLevel++;
+
+        if (_targetRenderer.Target.hasMultipleDifferentValues)
         {
-            var renderer = target as Renderer;
-            if (renderer == null) return null;
-
-            var material = renderer.sharedMaterial;
-            if (material == null) return null;
-
-            return material.shader;
+            // Multiple renderers selected: Show the simple text field.
+            EditorGUILayout.
+              PropertyField(_targetMaterialProperty, Labels.Property);
+        }
+        else if (_targetRenderer.Target.objectReferenceValue != null)
+        {
+            // Single renderer: Show the material property selection dropdown.
+            MaterialPropertySelector.
+              DropdownList(_targetRenderer, _targetMaterialProperty);
         }
 
-        // Cache the properties of the given shader .
-        void CachePropertyList(Shader shader)
-        {
-            // Do nothing if the shader is same to the cached one.
-            if (_cachedShader == shader) return;
+        EditorGUI.indentLevel--;
 
-            var temp = new List<string>();
-
-            var count = ShaderUtil.GetPropertyCount(shader);
-            for (var i = 0; i < count; i++)
-            {
-                var propType = ShaderUtil.GetPropertyType(shader, i);
-                if (propType == ShaderUtil.ShaderPropertyType.TexEnv)
-                    temp.Add(ShaderUtil.GetPropertyName(shader, i));
-            }
-
-            _propertyList = temp.ToArray();
-            _cachedShader = shader;
-        }
-
-        // Material property drop-down list
-        void ShowMaterialPropertyDropDown()
-        {
-            // Try retrieving the target shader.
-            var shader = RetrieveTargetShader(_targetRenderer.objectReferenceValue);
-
-            if (shader == null)
-            {
-                _targetMaterialProperty.stringValue = ""; // reset on failure
-                return;
-            }
-
-            // Cache the properties of the target shader.
-            CachePropertyList(shader);
-
-            // Check if there is suitable candidate.
-            if (_propertyList.Length == 0)
-            {
-                _targetMaterialProperty.stringValue = ""; // reset on failure
-                return;
-            }
-
-            // Show the drop-down list.
-            var index = Array.IndexOf(_propertyList, _targetMaterialProperty.stringValue);
-            var newIndex = EditorGUILayout.Popup("Property", index, _propertyList);
-
-            // Update the property if the selection was changed.
-            if (index != newIndex)
-                _targetMaterialProperty.stringValue = _propertyList[newIndex];
-        }
-
-        void OnEnable()
-        {
-            _appName = serializedObject.FindProperty("_appName");
-            _serverName = serializedObject.FindProperty("_serverName");
-            _targetTexture = serializedObject.FindProperty("_targetTexture");
-            _targetRenderer = serializedObject.FindProperty("_targetRenderer");
-            _targetMaterialProperty = serializedObject.FindProperty("_targetMaterialProperty");
-        }
-
-        void OnDisable()
-        {
-            _propertyList = null;
-            _cachedShader = null;
-        }
-
-        public override void OnInspectorGUI()
-        {
-            serializedObject.Update();
-
-            EditorGUI.BeginChangeCheck();
-
-            EditorGUILayout.DelayedTextField(_appName);
-            EditorGUILayout.DelayedTextField(_serverName);
-
-            // Force reconnection on modification to name properties.
-            if (EditorGUI.EndChangeCheck())
-                foreach (MonoBehaviour client in targets)
-                    client.SendMessage("OnDisable");
-
-            EditorGUILayout.PropertyField(_targetTexture);
-            EditorGUILayout.PropertyField(_targetRenderer);
-
-            EditorGUI.indentLevel++;
-
-            if (_targetRenderer.hasMultipleDifferentValues)
-            {
-                // Show a simple text field if there are multiple values.
-                EditorGUILayout.PropertyField(_targetMaterialProperty, _labelProperty);
-            }
-            else if (_targetRenderer.objectReferenceValue != null)
-            {
-                // Show the material property drop-down list.
-                ShowMaterialPropertyDropDown();
-            }
-
-            EditorGUI.indentLevel--;
-
-            serializedObject.ApplyModifiedProperties();
-        }
+        serializedObject.ApplyModifiedProperties();
     }
 }
+
+} // namespace Klak.Syphon
